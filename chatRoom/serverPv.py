f0c11e56de70f82f88513fe9e6ce8da0d924447b
arash <arash.ataee76@yahoo.com>
2019-12-12 00:56:33 +0330

    pvchat conected to sqlite3

--- chatRoom/serverPv.py
+++ chatRoom/serverPv.py
@@ -1,6 +1,71 @@
 import socket
 import time
 import select
+import sqlite3
+from sqlite3 import Error
+import datetime
+
+
+def create_connection(db_file):
+    connection = None
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+
+    return connection
+
+
+def create_table(connection, create_table_sql):
+    try:
+        c = connection.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def create_user(connection, u):
+    sql = ''' INSERT INTO users(name,socket,status)
+              VALUES(?,?,?) '''
+    cur = connection.cursor()
+    cur.execute(sql, u)
+    return cur.lastrowid
+
+
+def create_chat(connection, ch):
+    sql = ''' INSERT INTO chats(sender_name,receiver_name,chat_time)
+              VALUES(?,?,?) '''
+    cur = connection.cursor()
+    cur.execute(sql, ch)
+    return cur.lastrowid
+
+
+database = r"pvchat.db"
+
+sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
+                                    id integer PRIMARY KEY AUTOINCREMENT,
+                                    name text NOT NULL,
+                                    socket text NOT NULL,
+                                    status text NOT NULL
+                                ); """
+
+sql_create_chats_table = """CREATE TABLE IF NOT EXISTS chats (
+                                id integer PRIMARY KEY AUTOINCREMENT,
+                                sender_name text NOT NULL,
+                                receiver_name text NOT NULL,
+                                chat_time text NOT NULL
+                            );"""
+
+conn = create_connection(database)
+
+if conn is not None:
+    create_table(conn, sql_create_users_table)
+
+    create_table(conn, sql_create_chats_table)
+else:
+    print("Error! cannot create the database connection.")
+
 
 IP = '192.168.1.52'
 PORT = 8222
@@ -44,6 +109,9 @@ while True:
                 if username not in clients:
                     s.send(bytes('accept1', 'utf-8'))
                     clients[username] = s
+                    with conn:
+                        user = (username, str(s), 'online')
+                        create_user(conn, user)
                     names = ''
                     for name in list(clients.keys()):
                         names += name + ' '
@@ -60,19 +128,26 @@ while True:
                 if message.startswith('adnc'):
                     audience = message[4:]
                     if audience in audiences:
-                        s.send(bytes('error2', 'utf-8'))
-                        message = None
+                        if audiences[audience] != s:
+                            s.send(bytes('error2', 'utf-8'))
+                            message = None
+                        else:
+                            s.send(bytes('accept2', 'utf-8'))
+                            message = None
                     else:
                         s.send(bytes('accept2', 'utf-8'))
                         audiences[s] = audience
                         audiences[audience] = s
                         message = None
                 if message:
-                    print(message)
-                    if not message:
-                        socket_list.remove(s)
-                    clients[audiences[s]].send(bytes(list(clients.keys())[list(clients.values()).index(s)] + ': ' +
-                                                     message + '\n', 'utf-8'))
+                    sender_name = list(clients.keys())[list(clients.values()).index(s)]
+                    receiver_name = audiences[s]
+                    chat_time = datetime.datetime.now()
+                    chat = (sender_name, receiver_name, str(chat_time))
+                    with conn:
+                        create_chat(conn, chat)
+                    clients[receiver_name].send(
+                        bytes(sender_name + ': ' + message + '\n', 'utf-8'))
 
     for s in exception_socket:
         socket_list.remove(s)
