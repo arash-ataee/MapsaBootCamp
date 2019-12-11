859138f1d1736cdaa726aefa54ebb9b1b8363e33
arash <arash.ataee76@yahoo.com>
2019-12-11 19:25:59 +0330

    created gui for pvchat

--- chatRoom/clientpv.py
+++ chatRoom/clientpv.py
@@ -1,65 +1,158 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
 import socket
 import select
+import threading
 import time
 import sys
 
-IP = '192.168.1.53'
-PORT = 8220
-username = input("Enter your username: ")
+IP = '192.168.1.52'
+PORT = 8222
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.connect((IP, PORT))
-server_socket.setblocking(1)
+server_socket.setblocking(0)
 
 socket_list = [server_socket]
 
+root = Tk()
+root.title('arash chat')
+root.resizable(False, False)
 
 
+def chat_win():
+    def receive():
+        while True:
+            read_socket, write_socket, excepted_socket = select.select(socket_list, socket_list, socket_list)
+            for s in read_socket:
+                r_message = s.recv(1024)
+                if r_message:
+                    chat_list.insert('end', r_message.decode('utf-8'))
+
+                if not r_message:
+                    socket_list.remove(s)
+                    messagebox.showinfo(message="Connection Closed!", title='connection error')
+            time.sleep(1)
+
+    def send():
+        message = message_var.get()
+        chat_list.insert('end', 'you: ' + message)
+        server_socket.send(bytes(message, 'utf-8'))
+
+    chat_frame = ttk.Frame(root, padding=(50, 50, 62, 62))
+    chat_frame.grid(column=0, row=0)
+    chat_list = Listbox(chat_frame, height=10)
+    chat_list.grid(column=1, row=1, sticky=(S, W, E, N))
+    s = Scrollbar(chat_frame, orient=VERTICAL, command=chat_list.yview)
+    s.grid(column=2, row=1, sticky=(N, S))
+    chat_list['yscrollcommand'] = s.set
+    message_var = StringVar()
+    msg = Entry(chat_frame, width=20, textvariable=message_var)
+    msg.grid(column=1, row=2)
+    send_msg = Button(chat_frame, text='send', command=send)
+    send_msg.grid(column=2, row=2)
+    t1 = threading.Thread(target=receive)
+    t1.start()
 
 
-server_socket.send(bytes(username, 'utf8'))
-error1 = None
-while not error1:
-    try:
-        while not error1:
-            error1 = server_socket.recv(1024).decode('utf-8')
-    except IOError as e:
-        pass
-    if error1 == 'error1':
-        username = input("Enter other username: ")
-        server_socket.send(bytes(username, 'utf8'))
-        error1 = None
-
-else:
-    audience = input("Enter audience username: ")
-    server_socket.send(bytes(audience, 'utf-8'))
-    error2 = None
-    while not error2:
-        try:
-            while not error2:
+def match_win(audience_names):
+    def aud():
+        print(audience_var.get())
+        aud_name = audience_var.get()
+        print(aud_name)
+        server_socket.send(bytes('adnc' + aud_name, 'utf-8'))
+        error2 = None
+        while not error2:
+            try:
                 error2 = server_socket.recv(1024).decode('utf-8')
-        except IOError as e:
-            pass
+                print(error2)
+            except IOError as e:
+                continue
         if error2 == 'error2':
-            audience = input('{} is offline enter other audience username: '.format(audience))
-            server_socket.send(bytes(audience, 'utf-8'))
-            error2 = None
-        if error2 == 'wait':
-            print('please wait')
+            return False
+        if error2.startswith('accept2'):
+            return True
 
-while True:
-    read_socket, write_socket, excepted_socket = select.select(socket_list, socket_list, socket_list)
-    print(read_socket)
-    for s in read_socket:
-        msg = s.recv(1024)
-        if msg:
-            print(msg.decode('utf-8'))
-        if not msg:
-            socket_list.remove(s)
-            print("Connection Closed!")
+    def aud_check():
+        if aud():
+            chat_win()
+        else:
+            messagebox.showinfo(message="the selected person is busy, select other person: ", title='audience error')
 
-    for s in write_socket:
-        message = input('->')
-        s.send(bytes(message, 'utf-8'))
+    match_frame = ttk.Frame(root, padding=(50, 50, 62, 62))
+    match_frame.grid(column=0, row=0)
+    name_label = Label(match_frame, text='select an online person: ')
+    name_label.grid(column=1, row=1)
+    audience_var = StringVar()
+    audience = ttk.Combobox(match_frame, textvariable=audience_var)
+    audience['values'] = audience_names.split()
+    audience.grid(column=2, row=1)
+    set_name = Button(match_frame, text='start chat', command=aud_check)
+    set_name.grid(column=2, row=2)
 
+
+def login_win():
+    def login():
+        user_name = username.get()
+        print(user_name)
+        server_socket.send(bytes(user_name, 'utf-8'))
+        global error1
+        error1 = None
+        while not error1:
+            time.sleep(1)
+            try:
+                error1 = server_socket.recv(1024).decode('utf-8')
+                print(error1)
+            except IOError as e:
+                continue
+        if error1 == 'error1':
+            return False
+        if error1.startswith('accept1'):
+            return True
+
+    def login_check():
+        user_name = username.get()
+        if ' ' not in user_name:
+            if login():
+                audience_names = error1[7:]
+                match_win(audience_names)
+            else:
+                messagebox.showinfo(message="Enter other username: ", title='username error')
+        else:
+            messagebox.showinfo(message='do not use space in your username', title='username error')
+
+    login_frame = ttk.Frame(root, padding=(50, 50, 62, 62))
+    login_frame.grid(column=0, row=0)
+    name_label = Label(login_frame, text='username: ')
+    name_label.grid(column=1, row=1)
+    username = StringVar()
+    name = Entry(login_frame, width=20, textvariable=username)
+    name.grid(column=2, row=1)
+    set_name = Button(login_frame, text='set username', command=login_check)
+    set_name.grid(column=2, row=2)
+
+
+login_win()
+
+
+def match(audiences):
+    for audience in audiences:
+        server_socket.send(bytes(audience, 'utf-8'))
+        error2 = None
+        while not error2:
+            try:
+                while not error2:
+                    error2 = server_socket.recv(1024).decode('utf-8')
+            except IOError as e:
+                pass
+            if error2 == 'error2':
+                audience = input('{} is offline enter other audience username: '.format(audience))
+                server_socket.send(bytes(audience, 'utf-8'))
+                error2 = None
+            if error2 == 'wait':
+                print('please wait')
+
+
+root.mainloop()
 #    time.sleep(5)
--- chatRoom/serverPv.py
+++ chatRoom/serverPv.py
@@ -2,11 +2,11 @@ import socket
 import time
 import select
 
-IP = ''
-PORT = 8220
+IP = '192.168.1.52'
+PORT = 8222
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((IP, PORT))
 
 server_socket.listen(10)
@@ -17,56 +17,69 @@ socket_list = [server_socket]
 clients = {}
 audiences = {}
 
-
 while True:
     read_socket, write_socket, exception_socket = select.select(
-        socket_list, [], socket_list)
+        socket_list, socket_list, socket_list)
+    print('read', read_socket)
+    print('write', write_socket)
     for s in read_socket:
         if s == server_socket:
             client_socket, address = server_socket.accept()
             if client_socket:
                 # client_socket.send(bytes("welcome!", 'utf-8'))
                 socket_list.append(client_socket)
-                username = client_socket.recv(1024).decode('utf-8')
-                print(username)
-                while username in clients:
-                    client_socket.send(bytes('error1'))
-                    username = client_socket.recv(1024)
-                client_socket.send(bytes('accepted1', 'utf-8'))
-                clients[username] = client_socket
                 print("Connection Established from {}".format(address))
-
-
-
-
+    print('read', read_socket)
+    print('write', write_socket)
+    for s in read_socket:
+        if s not in clients.values():
+            username = None
+            try:
+                username = s.recv(1024).decode('utf-8')
+            except IOError:
+                continue
+            if username:
+                if username in clients:
+                    s.send(bytes('error1', 'utf-8'))
+                if username not in clients:
+                    s.send(bytes('accept1', 'utf-8'))
+                    clients[username] = s
+                    names = ''
+                    for name in list(clients.keys()):
+                        names += name + ' '
+                    s.send(bytes(names, 'utf-8'))
+                    print(clients)
+                    print(audiences)
         else:
-            if s not in audiences:
-                audience = None
-                if not audience:
-                    try:
-                        if not audience:
-                            audience = s.recv(1024).decode('utf-8')
-                    except IOError as e:
-                        pass
-                    if audience not in clients:
-                        s.send(bytes('error2', 'utf-8'))
-                        audience = None
-                    else:
-                        s.send(bytes('wait', 'utf-8'))
-                        audiences[s] = audience
-            else:
+            message = None
+            try:
                 message = s.recv(1024).decode('utf-8')
-                print(message)
-                if not message:
-                    socket_list.remove(s)
-                    del clients[s]
-                    continue
-                clients[audiences[s]].send(bytes(message + '\n', 'utf-8'))
+            except IOError:
+                continue
+            if message:
+                if message.startswith('adnc'):
+                    audience = message[4:]
+                    if audience in audiences:
+                        s.send(bytes('error2', 'utf-8'))
+                        message = None
+                    else:
+                        s.send(bytes('accept2', 'utf-8'))
+                        audiences[s] = audience
+                        audiences[audience] = s
+                        message = None
+                if message:
+                    print(message)
+                    if not message:
+                        socket_list.remove(s)
+                    clients[audiences[s]].send(bytes(list(clients.keys())[list(clients.values()).index(s)] + ': ' +
+                                                     message + '\n', 'utf-8'))
 
     for s in exception_socket:
         socket_list.remove(s)
         del clients[s]
-        del audiences[s]
         del audiences[audiences[s]]
+        del audiences[s]
+
+    time.sleep(1)
     # print(socket_list)
 # server_socket.close()
